6ece75d699c8a8a395499dd8f735dd462bf8b972
easterdaymn <148877722+easterdaymn@users.noreply.github.com>
2023-12-03 21:06:27 -0500

    Create courses.js

---
+++ models/courses.js
@@ -0,0 +1,14 @@
+// courses.js
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const courseSchema = new Schema({
+  courseName: { type: String, required: true },
+  description: { type: String, required: true },
+  subjectArea: { type: String, required: true },
+  credits: { type: Number, required: true },
+}, { timestamps: true });
+
+const Course = mongoose.model('Course', courseSchema);
+
+module.exports = Course;
