75a47a5a61f459b75b2479771a1964c1d59c2619
easterdaymn <148877722+easterdaymn@users.noreply.github.com>
2023-12-15 20:33:08 -0500

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,24 +3,23 @@ const jwt = require('jsonwebtoken');
 const requireAuth = (req, res, next) => {
   const token = req.cookies.jwt;
 
-  // check json web token exists & is verified
   if (token) {
     jwt.verify(token, 'okapifinalproject', async (err, decodedToken) => {
       if (err) {
         console.error('Token verification failed:', err.message);
         res.redirect('/login');
       } else {
-        // Assuming decodedToken contains user details, including the role
         const { role } = decodedToken;
 
-        // Add a check for the user's role
-        if (role === 'teacher' || role === 'student') {
-          // User has the required role, proceed to the next middleware or route
-          req.user = decodedToken; // Attach user details to the request
+        console.log('Decoded Token:', decodedToken); 
+        console.log('User Role:', role); 
+
+        if (role === 'student' || role === 'teacher') {
+          req.user = decodedToken;
           next();
         } else {
-          // Unknown role or unexpected value
-          res.status(403).send('Forbidden: Unknown role or unexpected value.');
+          console.log('Forbidden: Access denied for non-students and non-teachers.'); 
+          res.status(403).send('Forbidden: Access denied for non-students and non-teachers.');
         }
       }
     });
@@ -28,6 +27,7 @@ const requireAuth = (req, res, next) => {
     res.redirect('/login');
   }
 };
+
 
 module.exports = { requireAuth };
 
