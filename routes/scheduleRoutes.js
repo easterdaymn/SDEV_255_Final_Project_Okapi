da971be488bd986c7c5be4c3b64fd91da7752c4a
easterdaymn <148877722+easterdaymn@users.noreply.github.com>
2023-12-15 20:29:07 -0500

    Update scheduleRoutes.js

--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 const scheduleController = require('../controllers/scheduleController');
 
+// Rendering the /students page
+router.get('/students', authMiddleware.requireAuth, scheduleController.renderStudentsPage);
+
 // Adding a course
 router.post('/add/:id', authMiddleware.requireAuth, scheduleController.addToSchedule);
 
