41dd4850a8455711bd637efd643d5e4d8cda10b4
easterdaymn <148877722+easterdaymn@users.noreply.github.com>
2023-12-15 20:34:04 -0500

    Update scheduleController.js

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -1,4 +1,23 @@
 const User = require('../models/User');
+const Course = require('../models/courses');
+
+const renderStudentsPage = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    // Fetch the user's schedule
+    const user = await User.findById(userId);
+
+    // Fetch all available courses
+    const allCourses = await Course.find();
+
+    // Render the page with the user and course information
+    res.render('students', { title: 'Students', user, courses: allCourses });
+  } catch (err) {
+    console.error('Error rendering /students page:', err);
+    res.status(500).send('Internal Server Error');
+  }
+};
 
 const addToSchedule = async (req, res) => {
   const courseId = req.params.id;
@@ -7,12 +26,16 @@ const addToSchedule = async (req, res) => {
   try {
     // Add the course to the user's schedule
     await User.findByIdAndUpdate(userId, { $addToSet: { schedule: courseId } });
-
+  
+    // Fetch the updated user information and courses
+    const user = await User.findById(userId);
+    const courses = await Course.find();
+  
     // Redirect back to the /students page with the updated schedule
-    res.redirect('/students');
+    res.render('students', { title: 'Students', user, courses });
   } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
+    console.error('Error in addToSchedule:', err);
+    res.status(500).send('Internal Server Error: Unable to update schedule.');
   }
 };
 
@@ -36,6 +59,7 @@ const removeFromSchedule = async (req, res) => {
 };
 
 module.exports = {
+  renderStudentsPage,
   addToSchedule,
   removeFromSchedule,
 };
